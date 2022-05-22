e07d235612b499681627785fdae83444eb60bbc0
FeduuML <fedeliber@live.com>
2022-05-22 18:47:08 -0300

    -

--- signup.php
+++ signup.php
@@ -2,25 +2,32 @@
 <html>
     <head>
         <title>Signup</title>
+        <link href="signup.css" rel="stylesheet" type="text/css">
     </head>
 
     <body>
-        <?php require 'header.php'?>
-
+        
         <?php if(!empty($message)): ?>
             <p><?= $message ?></p>
         <?php endif; ?>
-
+        
+        <section class="registro">
         <form action="signup.php" method="post">
-            <h1>Signup</h1>
-            <input type="text" name="username" placeholder="Nombre de usuario">
-            <input type="text" name="email" placeholder="Correo electronico">
-            <input type="password" name="password" placeholder="Contraseña">
-            <input type="password" name="confirm_password" placeholder="Confirmar contraseña">
-            <input type="submit" value="Registrarse">
+            <h1 class="signup">Signup</h1>
+            <input class="controls" type="text" name="username" placeholder="Nombre de usuario">
+            <br>
+            <input class="controls" type="text" name="email" placeholder="Correo electronico">
+            <br>
+            <input class="controls" type="password" name="password" placeholder="Contraseña">
+            <br>
+            <input class="controls" type="password" name="confirm_password" placeholder="Confirmar contraseña">
+            <br>
+            <br>
+            <input class="control_registrarse" type="submit" value="Registrarse">
             <br><br>
-            <a href="index.php">Iniciar sesion</a>
+            <p class="iniciar_sesion"> ¿Ya tenes cuenta? <a href="index.php">Iniciar sesion</a></p>
         </form>
+        </section>
     </body>
 </html>
 
@@ -37,7 +44,7 @@
         $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
         $stmt->bindParam(':password', $password);
 
-        if ($_POST["password"] == $_POST["confirm_password"]){
+        if ($_POST["password"] == $_PpOST["confirm_password"]){
             if($stmt->execute()) {
                 $message = 'La cuenta ha sido exitosamente creada';
             }else{
