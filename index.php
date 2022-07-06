4fca8949e2c9c5b2ed0deea1fa1b40682d6adb6b
MaxxiChaves <87017818+MaxxiChaves@users.noreply.github.com>
2022-07-06 10:43:18 -0300

    Agregacion de elementos al index 2
    *Se cambio la fuente de texto.
    *se acomodaron las cajas de navegación.
    *Se modificaron los tamaños de las cajas.
    *Se agrego una caja contenedora.
    *Modificaciones en los hovers de las cajas de navegacion.

--- index.php
+++ index.php
@@ -13,10 +13,12 @@
         <title>Qatar Clic</title>
         <link href="styles/main.css" rel="stylesheet" type="text/css">
         <link href="styles/header.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css2?family=Lobster+Two&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Lobster+Two&family=Yanone+Kaffeesatz:wght@300&display=swap" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script> <!--Source de los iconos-->
     </head>
-    
+   
     <body>
        <header class="header">
             <div class="wrapper">
@@ -55,25 +57,27 @@
             </nav>
         </header>
         
-      
+        <article class="navegador_general">
             
             <div class="noticias">
                 <h1 class="text">Noticias</h1>
             </div>
 
-            <div class="partidos">
-                <h1 class="text">Partidos</h1>
+            <div class="fixture">
+                <h1 class="text">Fixture</h1>
             </div> 
             
-            <div class="qatar">
-                <h1 class="text">Qatar<h1>
-            </div>
-
             <div class="calendario">
                 <h1 class="text">Calendario</h1>
             </div>
 
-        
+            <div class="qatar">
+                <h1 class="text">Qatar<h1>
+            </div>
+
+        </article>    
+
+                
             
     </body>
 </html>
