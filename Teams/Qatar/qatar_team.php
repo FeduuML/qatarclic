4bb4623ac6339aa78aa3d73a2a8f435eaeae740f
Lolo959 <70668715+Lolo959@users.noreply.github.com>
2022-07-14 17:20:02 -0300

    Lista del equipo de Qatar

---
+++ Teams/Qatar/qatar_team.php
@@ -0,0 +1,78 @@
+<html>
+    <head>
+        <title>Qatar clic</title>
+        <link href="../Teams/Qatar/qatar_team.css" rel="stylesheet" type="text/css">
+        <link href="../styles/header.css" rel="stylesheet" type="text/css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head> 
+
+    <body>
+
+<h1> Qatar </h1>
+
+        <h2> ARQUEROS </h2>
+
+
+<h2><img src="../Qatar/images/1.jpg">1 - Saad Abdullah Al Sheeb    32 años</h2>
+<h2><img src="../Qatar/images/21.jpg">21 - Yousef Hassan Mohamed Ali   26 años</h2>
+<h2><img src="../Qatar/images/22.jpg">22 - Meshaal Aissa Barsham   24 años</h2>
+
+        <h2> DEFENSORES </h2>
+
+<h2><img src="../Qatar/images/2.jpg">2 - Pedro Miguel Correia   31 años</h2>
+<h2><img src="../Qatar/images/3.jpg">3 - Abdelkarim Hassan Al Haj Fadlalla   28 años</h2>
+<h2><img src="../Qatar/images/4.jpg">4 - Mohammed Waed Abdulwahhab Al Bayati   22 años</h2>
+<h2><img src="../Qatar/images/7.jpg">7 - Salem Ali Al Hajri  26 años</h2>
+<h2><img src="../Qatar/images/13.jpg">13 - Mosaab Khoder Jibril   28 años</h2>
+<h2><img src="../Qatar/images/15.jpg">15 - Bassam Hisham Al Rawi  24 años</h2>
+<h2><img src="../Qatar/images/16.jpg">16 - Boualem Khoukhi	Boualem Khoukhi   32 años</h2>
+
+<h2> MEDIOCAMPISTAS </h2>
+
+<h2><img src="../Qatar/images/8.jpg">8 - Ahmed FathyAhmed Fathy   29 años</h2>
+<h2><img src="../Qatar/images/5.jpg">5 - Tarek Salman Salman   24 años</h2>
+<h2><img src="../Qatar/images/6.jpg">6 - Hazem Ahmed Shehata   24 años</h2>
+<h2><img src="../Qatar/images/8.jpg">8 - Ali Assadalla Thaimn Qambar  29 años</h2>
+<h2><img src="../Qatar/images/9.jpg">9 - Abdulrahman Mohamed Fahmi Moustafa   25 años</h2>
+<h2><img src="../Qatar/images/12.jpg">12 - Karim Boudiaf   31 años</h2>
+<h2><img src="../Qatar/images/14.jpg">14 - Homam Alamin Ahmed   22 años</h2>
+<h2><img src="../Qatar/images/18.jpg">18 - Naif Abdulraheem Al Hadhrami   22 años</h2>
+<h2><img src="../Qatar/images/20.jpg">20 - Abdullah Abdulsalam Alahrak   25 años</h2>
+<h2><img src="../Qatar/images/23ahmed.jpg">23 - Assim Omer Al Haj Madibo   25 años</h2>
+<h2><img src="../Qatar/images/23.jpg">23 - Mohammed Alaa Eldin Abdelmotaal   28 años</h2>
+
+<h2> DELANTEROS </h2>
+
+<h2><img src="../Qatar/images/7delantero.jpg">7 - Ahmed Alaa Eldin Abdelmotaal   29 años</h2>
+<h2><img src="../Qatar/images/10.jpg">10 - Khalid Muneer Mazeed   24 años</h2>
+<h2><img src="../Qatar/images/11.jpg">11 - Akram Hassan Afif   25 años</h2>
+<h2><img src="../Qatar/images/17.jpg">17 - Ismaeel Mohammad Mohammad   32 años</h2>
+<h2><img src="../Qatar/images/18.jpg">18 - Yusuf Abdurisag   22 años</h2>
+<h2><img src="../Qatar/images/19.jpg">19 - Almoez Ali Zainalabiddin Abdulla   25 años</h2>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
