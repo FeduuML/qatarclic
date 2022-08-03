a12b107eaacfb1c6c0f9ed1484c5bbf2e433aaf3
matefer <89546062+matefer@users.noreply.github.com>
2022-08-03 16:12:04 -0300

    senegal

---
+++ Teams/Ecuador/senegal-team.php
@@ -0,0 +1,53 @@
+<html>
+    <head>
+        <title>Qatar clic</title>
+        <link href="../Teams/Senegal/qatar_team.css" rel="stylesheet" type="text/css">
+        <link href="../styles/header.css" rel="stylesheet" type="text/css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head> 
+
+    <body>
+
+<h1> Senegal </h1>
+
+        <h2> ARQUEROS </h2>
+
+
+<h2><img src="../Senegal/images/16.png">16 - Edouard Mendy  30 años</h2>
+<h2><img src="../Senegal/images/16alfred.png">16 - Alfred Gomis  28 años</h2>
+<h2><img src="../Senegal/images/1.png">1 - Seny Dieng 27 años</h2>
+
+        <h2> DEFENSORES </h2>
+
+<h2><img src="../Senegal/images/3.png">3 - 	Kalidou Koulibaly 31 años</h2>
+<h2><img src="../Senegal/images/22.png">22 - Abdou Diallo  26 años</h2>
+<h2><img src="../Senegal/images/4.png">4 - Pape Abou Cissé 26 años</h2>
+<h2><img src="../Senegal/images/14.png">14 - Abdoulaye Seck 30 años</h2>
+<h2><img src="../Senegal/images/12.png">12 - Fodé Ballo-Touré 25 años</h2>
+<h2><img src="../Senegal/images/2.png">2 - Saliou Ciss 32 años</h2>
+<h2><img src="../Senegal/images/12.png">12 - Youssouf Sabaly 29 años</h2>
+<h2><img src="../Senegal/images/2alpha.png">2 - Alpha Diounkou  20 años</h2>
+<h2><img src="../Senegal/images/26.png">26 - Pape Gueye  23 años</h2>
+
+
+<h2> MEDIOCAMPISTAS </h2>
+
+<h2><img src="../Senegal/images/6.png">6 - Nampalys Mendy 30 años</h2>
+<h2><img src="../Senegal/images/8.png">8 - Cheikhou Kouyaté 32 años</h2>
+<h2><img src="../Senegal/images/25.png">25 - Mamadou Loum 25 años</h2>
+<h2><img src="../Senegal/images/17.png">17 - Pape Matar Sarr 19 años</h2>
+<h2><img src="../Senegal/images/5.png">5 - Idrissa Gueye 32 años</h2>
+<h2><img src="../Senegal/images/24.png">24 - Moustapha Name 27 años</h2>
+<h2><img src="../Senegal/images/29.png">29 - Iliman Ndiaye  22 años</h2>
+<h2><img src="../Senegal/images/10.png">10 - Sadio Mané 30 años</h2>
+<h2><img src="../Senegal/images/18.png">18 - Ismaïla Sarr 24 años</h2>
+<h2><img src="../Senegal/images/23.png">23 - Demba Seck 21 años</h2>
+<h2><img src="../Senegal/images/9.png">9 - Boulaye Dia 25 años</h2>
+
+<h2> DELANTEROS </h2>
+
+<h2><img src="../Senegal/images/11.png">11 - Habib Diallo 27 años</h2>
+<h2><img src="../Senegal/images/19.png">19 - Famara Diédhiou 29 años</h2>
+<h2><img src="../Senegal/images/7.png">7 - Keita Baldé 27 años</h2>
+</body>
+</html>
