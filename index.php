4fb58731ff5d8d06b50cab25c3b690654994f4ca
matefer <89546062+matefer@users.noreply.github.com>
2022-08-04 15:01:00 -0300

    noticias

--- index.php
+++ index.php
@@ -113,7 +113,6 @@ $desde = @$_GET['pag'] * 20;
         while($news1 = $news->fetch(PDO::FETCH_ASSOC)):
             echo '<h1>'.$news1['title'].'</h1>';
             echo '<h2>'.$news1['content'].'</h2>';
-            echo '<img src="data:image/png;base64>';
-            echo base64_encode($news1['imagen']);
+            echo '<img src="data:image/jpg;base64, '.base64_encode($news1['image']);
         endwhile;
 ?>
