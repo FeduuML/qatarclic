fd5734fb08148eaeccea20cf058d85cce18b7092
Lolo959 <70668715+Lolo959@users.noreply.github.com>
2022-08-18 15:01:03 -0300

    selections

--- main/grupos/selections.php
+++ main/grupos/selections.php
@@ -12,76 +12,77 @@
 
                 <div class= "grupoa">
                     <h1> Grupo A </h1>
-                    <h2><img class="imagen_bandera"src="../../images/flags/qatar.jpg"><a href="../../teams/qatar/qatar_team.php">Qatar</a></h2>  
-                    <h2><img class="imagen_bandera"src="../../images/flags/ecuador.jpg"><a href="../../teams/Ecuador/ecuador-team.php">Ecuador</a></h2>
-                    <h2><img class="imagen_bandera"src="../../images/flags/senegal.jpg"><a href="../../teams/Senegal/senegal-team.php">Senegal</a></h2>
-                    <h2><img class="imagen_bandera"src="../../images/flags/holanda.jpg"><a href="../../teams/Senegal/senegal-team.php">Holanda</a></h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/qatar.jpg"><a href="../../teams/qatar/qatar_team.php">Qatar</a></h2>  
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/ecuador.jpg"><a href="../../teams/Ecuador/ecuador-team.php">Ecuador</a></h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/senegal.jpg"><a href="../../teams/Senegal/senegal-team.php">Senegal</a></h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/holanda.jpg"><a href="../../teams/Senegal/senegal-team.php">Holanda</a></h2>
                 </div>
                     <br>
 
                 <div class= "grupoe">
                     <h1> Grupo E </h1>
-                    <h2><img class="imagen_bandera" src="../../images/flags/españa.jpg">España</h2>  
-                    <h2><img class="imagen_bandera"src="../../images/flags/costa_rica.jpg">Costa Rica</h2>
-                    <h2><img class="imagen_bandera"src="../../images/flags/alemania.jpg">Alemania</h2>
-                    <h2><img class="imagen_bandera"src="../../images/flags/japon.jpg">Japon</h2>
+                    <h2 class="contornos"><img class="imagen_bandera" src="../../images/flags/españa.jpg">España</h2>  
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/costa_rica.jpg">Costa Rica</h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/alemania.jpg">Alemania</h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/japon.jpg">Japon</h2>
                 </div>
                     <br>
             
 
                 <div class= "grupob">
                     <h1> Grupo B </h1>
-                    <h2><img class="imagen_bandera"src="../../images/flags/inglaterra.jpg">Inglaterra</h2>  
-                    <h2><img class="imagen_bandera"src="../../images/flags/iran.jpg">Iran</h2>
-                    <h2><img class="imagen_bandera"src="../../images/flags/eeuu.jpg">Estados Unidos</h2>
-                    <h2><img class="imagen_bandera"src="../../images/flags/gales.jpg">Gales</h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/inglaterra.jpg">Inglaterra</h2>  
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/iran.jpg">Iran</h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/eeuu.jpg">Estados Unidos</h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/gales.jpg">Gales</h2>
                 </div> 
                     <br>
 
                 <div class= "grupof">
                     <h1> Grupo F </h1>
-                    <h2><img class="imagen_bandera"src="../../images/flags/belgica.jpg">Belgica</h2>  
-                    <h2><img class="imagen_bandera"src="../../images/flags/canada.jpg">Canada</h2>
-                    <h2><img class="imagen_bandera"src="../../images/flags/marruecos.jpg">Marruecos</h2>
-                    <h2><img class="imagen_bandera"src="../../images/flags/croacia.jpg">Croacia</h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/belgica.jpg">Belgica</h2>  
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/canada.jpg">Canada</h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/marruecos.jpg">Marruecos</h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/croacia.jpg">Croacia</h2>
                 </div> 
                     <br>
                 
                 <div class= "grupoc">
                     <h1> Grupo C </h1>
-                    <h2><img class="imagen_bandera"src="../../images/flags/argentina.jpg">Argentina</h2>  
-                    <h2><img class="imagen_bandera"src="../../images/flags/arabia.jpg">Arabia Saudita</h2>
-                    <h2><img class="imagen_bandera"src="../../images/flags/mexico.jpg">Mexico</h2>
-                    <h2><img class="imagen_bandera"src="../../images/flags/polonia.jpg">Polonia</h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/argentina.jpg">Argentina</h2>  
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/arabia.jpg">Arabia Saudita</h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/mexico.jpg">Mexico</h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/polonia.jpg">Polonia</h2>
                 </div>   
                     <br>
 
                 <div class= "grupog">
                     <h1> Grupo G </h1>
-                    <h2><img class="imagen_bandera"src="../../images/flags/brasil.jpg">Brasil</h2>  
-                    <h2><img class="imagen_bandera"src="../../images/flags/serbia.jpg">Serbia</h2>
-                    <h2><img class="imagen_bandera"src="../../images/flags/suiza.jpg">Suiza</h2>
-                    <h2><img class="imagen_bandera"src="../../images/flags/camerun.jpg">Camerun</h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/brasil.jpg">Brasil</h2>  
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/serbia.jpg">Serbia</h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/suiza.jpg">Suiza</h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/camerun.jpg">Camerun</h2>
                 </div>
                     <br>
 
                 <div class="grupod">
                     <h1> Grupo D </h1>
-                    <h2><img class="imagen_bandera"src="../../images/flags/francia.jpg">Francia</h2>  
-                    <h2><img class="imagen_bandera"src="../../images/flags/australia.jpg">Australia</h2>
-                    <h2><img class="imagen_bandera"src="../../images/flags/dinamarca.jpg">Dinamarca</h2>
-                    <h2><img class="imagen_bandera"src="../../images/flags/tunez.jpg">Tunez</h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/francia.jpg">Francia</h2>  
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/australia.jpg">Australia</h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/dinamarca.jpg">Dinamarca</h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/tunez.jpg">Tunez</h2>
                 </div>
                     <br>
 
                 <div class="grupoh">
                     <h1> Grupo H </h1>
-                    <h2><img class="imagen_bandera"src="../../images/flags/portugal.jpg">Portugal</h2>  
-                    <h2><img class="imagen_bandera"src="../../images/flags/ghana.jpg">Ghana</h2>
-                    <h2><img class="imagen_bandera"src="../../images/flags/uruguay.jpg">Uruguay</h2>
-                    <h2><img class="imagen_bandera"src="../../images/flags/korea.jpg">Korea del Sur</h2>   
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/portugal.jpg">Portugal</h2>  
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/ghana.jpg">Ghana</h2>
+                    <h2 class="contornos"><img class="imagen_bandera"src="../../images/flags/uruguay.jpg">Uruguay</h2>
+                    <h2 class="contornos">  <img class="imagen_bandera"src="../../images/flags/korea.jpg">Korea del Sur</h2>   
                 </div>
                     <br>
-              
+                    
+}
 
     </html>
