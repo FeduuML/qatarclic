0ef295a9717989d18f9ce76bc8a603801d44cf1c
Lolo959 <70668715+Lolo959@users.noreply.github.com>
2022-09-18 16:27:50 -0300

    Update
    Organización de carpetas SobreQatar

---
+++ main/sobreqatar/Estadios/Estadios.php
@@ -0,0 +1,98 @@
+<html>
+    <head>
+        <title>Qatar clic</title>
+        <link href="Estadios.css" rel="stylesheet" type="text/css">
+        <link href="../../styles/header.css" rel="stylesheet" type="text/css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+     
+    <body>
+    <header>
+        <h1 id="title">Estadios</h1>
+    </header> 
+
+    <section>
+        <div class="caja" id="left">
+            <img id=photo src="qatar-fotos/lusail_estadio.jpg"> 
+            <h2>Estadio Lusail</h2>
+            <p> Estadio con capacidad para 80,000 espectadores,<br> que albergará la final <br>de la Copa Mundial de la FIFA Catar 2022™.</p>
+        </div>
+        
+        <div class="caja" id="right">
+            <img id=photo src="qatar-fotos/al_bait_estadio.jpg">
+            <h2>Estadio Al Bait</h2>
+            <p> Estadio con capacidad para 80,000 espectadores,<br> que albergará la final <br>de la Copa Mundial de la FIFA Catar 2022™.</p>
+        </div>
+
+    </section>
+
+    <section>
+        <div class="caja" id="left">
+            <img id=photo src="qatar-fotos/974_estadio.jpg"> 
+            <h2>Estadio 974</h2>
+            <p> Un estadio con capacidad para 40,000 espectadores que rinde homenaje a la tradición marítima de Catar.</p>
+        </div>
+        
+        <div class="caja" id="right">
+            <img id=photo src="qatar-fotos/khalifa_estadio.jpg">
+            <h2>Estadio Khalifa</h2>
+            <p> Estadio con capacidad para 40,000 espectadores, que ha sido la piedra angular de los deportes en Catar desde 1976.</p>
+        </div>
+    </section>
+    <section>
+        <div class="caja" id="left">
+            <img id=photo src="qatar-fotos/educity_estadio.jpg"> 
+            <h2>Estadio Educational City</h2>
+            <p> Estadio con capacidad para 40,000 espectadores rodeado por las instituciones educativas de primer nivel de Catar.</p>
+        </div>
+        
+        <div class="caja" id="right">
+            <img id=photo src="qatar-fotos/aljanoub_estadio.jpg">
+            <h2>Estadio Al Janoub</h2>
+            <p> Estadio con capacidad para 40,000 espectadores en la ciudad sureña de Al Wakrah, uno de los asentamientos humanos más antiguos de Catar.</p>
+        </div>
+    </section>
+
+    <section>
+        <div class="caja" id="left">
+            <img id=photo src="qatar-fotos/althumama_estadio.jpg"> 
+            <h2>Estadio Al Thumama</h2>
+            <p> Estadio con capacidad para 40,000 espectadores, inspirado en la tradicional "gahfiya", ubicado a 12 km al sur de Doha</p>
+        </div>
+        
+        <div class="caja" id="right">
+            <img id=photo src="qatar-fotos/ahmadbinali_estadio.jpg">
+            <h2>Estadio Ahmad Bin Ali</h2>
+            <p> Estadio con capacidad para 40,000 espectadores situado en los terrenos del antiguo estadio Ahmad bin Ali.</p>
+        </div>
+    </section>
+
+        <!--<div class="estadios">
+            
+            
+             
+            
+            <h2><img class="imagen_althumama"src="qatar-fotos/althumama_estadio.jpg"></h2> 
+            <h2><img class="imagen_ahmadbinali"src="qatar-fotos/ahmadbinali_estadio.jpg"></h2>  
+        </div>
+        
+        <div class= "descrip">
+            
+            
+            <h2 class="althumama">Estadio con capacidad para 40,000 espectadores, inspirado en la tradicional "gahfiya", ubicado a 12 km al sur de Doha</h2>
+            <h2 class="ahmadbinali">Estadio con capacidad para 40,000 espectadores situado en los terrenos del antiguo estadio Ahmad bin Ali.</h2>
+
+        </div>*\-->
+
+
+
+
+
+
+
+
+    </body>
+</html>
