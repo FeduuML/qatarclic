e0ddf1707b7a423a7abb69e034bab7f76980550e
Lolo959 <70668715+Lolo959@users.noreply.github.com>
2022-09-18 14:38:09 -0300

    Pagina Estadios
    Mejoras css

--- main/sobreqatar/SobreQatar.php
+++ main/sobreqatar/SobreQatar.php
@@ -3,11 +3,32 @@
         <title>Qatar clic</title>
         <link href="SobreQatar.css" rel="stylesheet" type="text/css">
         <link href="../../styles/header.css" rel="stylesheet" type="text/css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head> 
+    </head>
+     
     <body>
+    <header>
+        <h1 id="title">Estadios</h1>
+    </header> 
 
-        <div class="estadios">
+    <section>
+        <div class="caja" id="lusail">
+            <img id=photo_lusail src="qatar-fotos/lusail_estadio.jpg"> 
+            <h2>Estadio Lusail</h2>
+            <p> Estadio con capacidad para 80,000 espectadores,<br> que albergará la final <br>de la Copa Mundial de la FIFA Catar 2022™.</p>
+        </div>
+        
+        <div class="caja" id="al_bait">
+            <img id=photo_al_bait src="qatar-fotos/al_bait_estadio.jpg">
+            <h2>Estadio Al Bait</h2>
+            <p> Estadio con capacidad para 80,000 espectadores,<br> que albergará la final <br>de la Copa Mundial de la FIFA Catar 2022™.</p>
+        </div>
+
+    </section>
+        <!--<div class="estadios">
             
             <h2><img class="imagen_lusail"src="qatar-fotos/lusail_estadio.jpg"></h2> 
             <h2><img class="imagen_al_bait"src="qatar-fotos/al_bait_estadio.jpg"></h2> 
@@ -21,7 +42,7 @@
         
         <div class= "descrip">
             <h2 class="lusail"><br><br>ESTADIO LUSAIL<br><br>Estadio con capacidad para 80,000 espectadores,<br> que albergará la final <br>de la Copa Mundial de la FIFA Catar 2022™. <br>PARTIDOS<br> 6 x Partidos de Grupo<br>
-Octavos de Final<br>Cuartos de Final<br>Semifinal y Final </h2>
+            Octavos de Final<br>Cuartos de Final<br>Semifinal y Final </h2>
             <h2 class="al_bait">Estadio con capacidad para 60,000 espectadores ubicado en la ciudad de Al Khor, célebre por sus actividades de pesca y de buceo para encontrar perlas</h2>
             <h2 class="e974">Un estadio con capacidad para 40,000 espectadores que rinde homenaje a la tradición marítima de Catar.</h2>
             <h2 class="khalifa">Estadio con capacidad para 40,000 espectadores, que ha sido la piedra angular de los deportes en Catar desde 1976.</h2>
@@ -30,7 +51,7 @@ Octavos de Final<br>Cuartos de Final<br>Semifinal y Final </h2>
             <h2 class="althumama">Estadio con capacidad para 40,000 espectadores, inspirado en la tradicional "gahfiya", ubicado a 12 km al sur de Doha</h2>
             <h2 class="ahmadbinali">Estadio con capacidad para 40,000 espectadores situado en los terrenos del antiguo estadio Ahmad bin Ali.</h2>
 
-        </div>
+        </div>*\-->
 
 
 
