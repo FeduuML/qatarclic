973be52624b5b8dfae296d98aad72e91313ca277
Lolo959 <70668715+Lolo959@users.noreply.github.com>
2022-09-17 21:51:53 -0300

    Sobre Qatar
    Estructura y diseño base

---
+++ main/sobreqatar/SobreQatar.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+        <title>Qatar clic</title>
+        <link href="SobreQatar.css" rel="stylesheet" type="text/css">
+        <link href="../../styles/header.css" rel="stylesheet" type="text/css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head> 
+    <body>
+
+
+        <div class="estadios">
+            
+            <h2><img class="imagen_lusail"src="qatar-fotos/lusail_estadio.jpg"></h2> 
+            <h2><img class="imagen_al_bait"src="qatar-fotos/al_bait_estadio.jpg"></h2> 
+            <h2><img class="imagen_974"src="qatar-fotos/974_estadio.jpg"></h2> 
+            <h2><img class="imagen_khalifa"src="qatar-fotos/khalifa_estadio.jpg"></h2> 
+            <h2><img class="imagen_educity"src="qatar-fotos/educity_estadio.jpg"></h2> 
+            <h2><img class="imagen_aljanoub"src="qatar-fotos/aljanoub_estadio.jpg"></h2> 
+            <h2><img class="imagen_althumama"src="qatar-fotos/althumama_estadio.jpg"></h2> 
+            <h2><img class="imagen_ahmadbinali"src="qatar-fotos/ahmadbinali_estadio.jpg"></h2>  
+        </div>
+        
+        <div class= "descrip">
+            <h2 class="lusail">Estadio con capacidad para 80,000 espectadores, que albergará la final de la Copa Mundial de la FIFA Catar 2022™.</h2>
+            <h2 class="al_bait">Estadio con capacidad para 60,000 espectadores ubicado en la ciudad de Al Khor, célebre por sus actividades de pesca y de buceo para encontrar perlas</h2>
+            <h2 class="e974">Un estadio con capacidad para 40,000 espectadores que rinde homenaje a la tradición marítima de Catar.</h2>
+            <h2 class="khalifa">Estadio con capacidad para 40,000 espectadores, que ha sido la piedra angular de los deportes en Catar desde 1976.</h2>
+            <h2 class="educity">Estadio con capacidad para 40,000 espectadores rodeado por las instituciones educativas de primer nivel de Catar.</h2>
+            <h2 class="aljanoub">Estadio con capacidad para 40,000 espectadores en la ciudad sureña de Al Wakrah, uno de los asentamientos humanos más antiguos de Catar.</h2>
+            <h2 class="althumama">Estadio con capacidad para 40,000 espectadores, inspirado en la tradicional "gahfiya", ubicado a 12 km al sur de Doha</h2>
+            <h2 class="ahmadbinali">Estadio con capacidad para 40,000 espectadores situado en los terrenos del antiguo estadio Ahmad bin Ali.</h2>
+
+        </div>
+
+
+
+
+
+
+
+
+    </body>
+</html>
