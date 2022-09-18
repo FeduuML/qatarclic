ef084ef4afeace36eb391339bc25c4401ce61051
Lolo959 <70668715+Lolo959@users.noreply.github.com>
2022-09-18 16:21:14 -0300

    Update Estadios

--- main/sobreqatar/SobreQatar.php
+++ main/sobreqatar/SobreQatar.php
@@ -15,39 +15,73 @@
     </header> 
 
     <section>
-        <div class="caja" id="lusail">
-            <img id=photo_lusail src="qatar-fotos/lusail_estadio.jpg"> 
+        <div class="caja" id="left">
+            <img id=photo src="qatar-fotos/lusail_estadio.jpg"> 
             <h2>Estadio Lusail</h2>
             <p> Estadio con capacidad para 80,000 espectadores,<br> que albergará la final <br>de la Copa Mundial de la FIFA Catar 2022™.</p>
         </div>
         
-        <div class="caja" id="al_bait">
-            <img id=photo_al_bait src="qatar-fotos/al_bait_estadio.jpg">
+        <div class="caja" id="right">
+            <img id=photo src="qatar-fotos/al_bait_estadio.jpg">
             <h2>Estadio Al Bait</h2>
             <p> Estadio con capacidad para 80,000 espectadores,<br> que albergará la final <br>de la Copa Mundial de la FIFA Catar 2022™.</p>
         </div>
 
     </section>
+
+    <section>
+        <div class="caja" id="left">
+            <img id=photo src="qatar-fotos/974_estadio.jpg"> 
+            <h2>Estadio 974</h2>
+            <p> Un estadio con capacidad para 40,000 espectadores que rinde homenaje a la tradición marítima de Catar.</p>
+        </div>
+        
+        <div class="caja" id="right">
+            <img id=photo src="qatar-fotos/khalifa_estadio.jpg">
+            <h2>Estadio Khalifa</h2>
+            <p> Estadio con capacidad para 40,000 espectadores, que ha sido la piedra angular de los deportes en Catar desde 1976.</p>
+        </div>
+    </section>
+    <section>
+        <div class="caja" id="left">
+            <img id=photo src="qatar-fotos/educity_estadio.jpg"> 
+            <h2>Estadio Educational City</h2>
+            <p> Estadio con capacidad para 40,000 espectadores rodeado por las instituciones educativas de primer nivel de Catar.</p>
+        </div>
+        
+        <div class="caja" id="right">
+            <img id=photo src="qatar-fotos/aljanoub_estadio.jpg">
+            <h2>Estadio Al Janoub</h2>
+            <p> Estadio con capacidad para 40,000 espectadores en la ciudad sureña de Al Wakrah, uno de los asentamientos humanos más antiguos de Catar.</p>
+        </div>
+    </section>
+
+    <section>
+        <div class="caja" id="left">
+            <img id=photo src="qatar-fotos/althumama_estadio.jpg"> 
+            <h2>Estadio Al Thumama</h2>
+            <p> Estadio con capacidad para 40,000 espectadores, inspirado en la tradicional "gahfiya", ubicado a 12 km al sur de Doha</p>
+        </div>
+        
+        <div class="caja" id="right">
+            <img id=photo src="qatar-fotos/ahmadbinali_estadio.jpg">
+            <h2>Estadio Ahmad Bin Ali</h2>
+            <p> Estadio con capacidad para 40,000 espectadores situado en los terrenos del antiguo estadio Ahmad bin Ali.</p>
+        </div>
+    </section>
+
         <!--<div class="estadios">
             
-            <h2><img class="imagen_lusail"src="qatar-fotos/lusail_estadio.jpg"></h2> 
-            <h2><img class="imagen_al_bait"src="qatar-fotos/al_bait_estadio.jpg"></h2> 
-            <h2><img class="imagen_974"src="qatar-fotos/974_estadio.jpg"></h2> 
-            <h2><img class="imagen_khalifa"src="qatar-fotos/khalifa_estadio.jpg"></h2> 
-            <h2><img class="imagen_educity"src="qatar-fotos/educity_estadio.jpg"></h2> 
-            <h2><img class="imagen_aljanoub"src="qatar-fotos/aljanoub_estadio.jpg"></h2> 
+            
+             
+            
             <h2><img class="imagen_althumama"src="qatar-fotos/althumama_estadio.jpg"></h2> 
             <h2><img class="imagen_ahmadbinali"src="qatar-fotos/ahmadbinali_estadio.jpg"></h2>  
         </div>
         
         <div class= "descrip">
-            <h2 class="lusail"><br><br>ESTADIO LUSAIL<br><br>Estadio con capacidad para 80,000 espectadores,<br> que albergará la final <br>de la Copa Mundial de la FIFA Catar 2022™. <br>PARTIDOS<br> 6 x Partidos de Grupo<br>
-            Octavos de Final<br>Cuartos de Final<br>Semifinal y Final </h2>
-            <h2 class="al_bait">Estadio con capacidad para 60,000 espectadores ubicado en la ciudad de Al Khor, célebre por sus actividades de pesca y de buceo para encontrar perlas</h2>
-            <h2 class="e974">Un estadio con capacidad para 40,000 espectadores que rinde homenaje a la tradición marítima de Catar.</h2>
-            <h2 class="khalifa">Estadio con capacidad para 40,000 espectadores, que ha sido la piedra angular de los deportes en Catar desde 1976.</h2>
-            <h2 class="educity">Estadio con capacidad para 40,000 espectadores rodeado por las instituciones educativas de primer nivel de Catar.</h2>
-            <h2 class="aljanoub">Estadio con capacidad para 40,000 espectadores en la ciudad sureña de Al Wakrah, uno de los asentamientos humanos más antiguos de Catar.</h2>
+            
+            
             <h2 class="althumama">Estadio con capacidad para 40,000 espectadores, inspirado en la tradicional "gahfiya", ubicado a 12 km al sur de Doha</h2>
             <h2 class="ahmadbinali">Estadio con capacidad para 40,000 espectadores situado en los terrenos del antiguo estadio Ahmad bin Ali.</h2>
 
