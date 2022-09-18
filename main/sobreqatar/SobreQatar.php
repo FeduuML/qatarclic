63e243dbdbb100c108f9a28684487213739c090f
Lolo959 <70668715+Lolo959@users.noreply.github.com>
2022-09-18 18:55:18 -0300

    Updates SobreQatar(principal)

--- main/sobreqatar/SobreQatar.php
+++ main/sobreqatar/SobreQatar.php
@@ -1,12 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<title>Qatar clic</title>
+        <link href="SobreQatar.css" rel="stylesheet" type="text/css">
+        <link href="../../styles/header.css" rel="stylesheet" type="text/css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Lobster+Two&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Lobster+Two&family=Yanone+Kaffeesatz:wght@300&display=swap" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <body>
-    
+    <header>
+        <h1>Sobre Qatar</h1>
+        <div>Estadios</div>
+        <div>Estadios</div>
+    </header>
+
+    <section>
+    <div class="caja" id=left>
+        <h1 id="infocatar">La Copa Mundial de Fútbol de la FIFA Catar 2022 será la XXII vigésimo segunda edición de la Copa Mundial de Fútbol masculino organizada por la FIFA.
+            Esta edición se realizará desde el 21 de noviembre al 18 de diciembre de 2022 en Catar, que consiguió los derechos de organización el 2 de diciembre de 2010.</h1>
+            Qatar un Estado soberano árabe ubicado en el oeste de Asia y que ocupa la pequeña península de Catar en el este de la península arábiga.
+    </div>
+    </section>
 </body>
 </html>
