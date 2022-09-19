60b4d433e26155e79ae23cc069ddb6250b770cf8
Lolo959 <70668715+Lolo959@users.noreply.github.com>
2022-09-19 00:48:46 -0300

    Sobreqatar Update
    Animaciones mejoradas

--- main/sobreqatar/SobreQatar.php
+++ main/sobreqatar/SobreQatar.php
@@ -10,6 +10,9 @@
         <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Lobster+Two&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Lobster+Two&family=Yanone+Kaffeesatz:wght@300&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
         <link href="../../header.css" rel="stylesheet" type="text/css">
 <body>
