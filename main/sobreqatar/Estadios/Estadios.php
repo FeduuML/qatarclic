1d096fac44dcab974b4c4aa8af25631f5e3ccbf1
matefer <fernandezcaroumateo@gmail.com>
2022-09-26 16:16:40 -0300

    Carrusel

--- main/sobreqatar/Estadios/Estadios.php
+++ main/sobreqatar/Estadios/Estadios.php
@@ -7,7 +7,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
         
     </head>
      
@@ -91,47 +91,32 @@
 navegador-->
         
 
-    <div>
-        <div class="caja">
-            <div class="caja-slider" id="slider">
- 
-                <div class="slider-section">
-                    <img id=photo src="qatar-fotos/lusail_estadio2.jpg" alt="" class="slider-img"> 
+<div class="caja">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="qatar-fotos/lusail_estadio2.jpg" class="d-block w-100" alt="...">
                 </div>
-                <div class="slider-section">
-                    <img id=photo src="qatar-fotos/lusail_estadio3.jpg" alt="" class="slider-img"> 
+                <div class="carousel-item">
+                <img src="qatar-fotos/lusail_estadio3.jpg" class="d-block w-100" alt="...">
                 </div>
-                <div class="slider-section">
-                    <img id=photo src="qatar-fotos/lusail_estadio.jpg" alt="" class="slider-img"> 
+                <div class="carousel-item">
+                <img src="qatar-fotos/lusail_estadio.jpg" class="d-block w-100" alt="...">
                 </div>
-        </div>
-            <div class="slider-btn slider-btn-right" id="btn-right">&#62</div>
-            <div class="slider-btn slider-btn-left" id="btn-left">&#60</div>
-            <h2>Estadio Lusail</h2>
-            <p> Estadio con capacidad para 80,000 espectadores,<br> que albergará la final <br>de la Copa Mundial de la FIFA Catar 2022™.</p>
-        </div>
-        
-                
-        <!--<div class="caja">
-        <div class="caja-slider" id="slider">
- 
-            <div class="slider-section">
-                <img id=photo src="qatar-fotos/lusail_estadio2.jpg" alt="" class="slider-img"> 
             </div>
-            <div class="slider-section">
-                <img id=photo src="qatar-fotos/lusail_estadio3.jpg" alt="" class="slider-img"> 
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
             </div>
-            <div class="slider-section">
-                <img id=photo src="qatar-fotos/lusail_estadio.jpg" alt="" class="slider-img"> 
-            </div>
-            </div>
-            <div class="slider-btn slider-btn-right" id="btn-right">&#62</div>
-            <div class="slider-btn slider-btn-left" id="btn-left">&#60</div>
-            <h2>Estadio Al Bait</h2>
-            <p> Estadio con capacidad para 80,000 espectadores,<br> que albergará la final <br>de la Copa Mundial de la FIFA Catar 2022™.</p>
-        </div>-->
-
-    </div>
+         <h2>Estadio Lusail</h2>
+        <p> Estadio con capacidad para 80,000 espectadores,<br> que albergará la final <br>de la Copa Mundial de la FIFA Catar 2022™.</p>
+</div>        
+    
 
     <section>
         <div class="caja" id="left">
@@ -173,7 +158,8 @@ navegador-->
             <p> Estadio con capacidad para 40,000 espectadores situado en los terrenos del antiguo estadio Ahmad bin Ali.</p>
         </div>
     </section>
-
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     </body>
 </html>
 <script>
@@ -181,4 +167,4 @@ navegador-->
         window.location.href="estadios.php";
     }
 </script>
-<script src="estadios.js"></script>
+<!--<script src="estadios.js"></script>-->
