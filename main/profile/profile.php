22c9d91e23e98d3f6f199a0f030feee1941cfb84
matefer <fernandezcaroumateo@gmail.com>
2022-10-03 17:11:27 -0300

    Perfil

--- main/profile/profile.php
+++ main/profile/profile.php
@@ -27,23 +27,9 @@
 		}
 	}
 
-	if(isset($_POST['btn-add2'])){
-		$images=$_FILES['image']['name'];
-			$tmp_dir=$_FILES['image']['tmp_name'];
-			$imageSize=$_FILES['image']['size'];
-			$upload_dir=dirname(__DIR__).'../../pfp/';
-			$imgExt=strtolower(pathinfo($images,PATHINFO_EXTENSION));
-			$valid_extensions=array('jpeg', 'jpg', 'png');
-			$pic=rand(1000, 1000000).".".$imgExt;
-			// move_uploaded_file($tmp_dir, $upload_dir.$picProfile);
-			move_uploaded_file($tmp_dir, $upload_dir.$pic);
-			$stmt=$conn->prepare("UPDATE users SET image = :uima WHERE id = $user_id");
-			$stmt->bindParam(':uima', $pic);
-			$stmt->execute();
-	}
 ?>
 
-<div class="bio">			
+	<div class="bio">			
 	<form method="post" class="upload" enctype="multipart/form-data">
 		<div class="bio">
 			<label class="label">Biografia</label>
@@ -55,18 +41,35 @@
 	</form>
 </div>
 
-<div class="pfp	">		
-	<p class="title">Agregar foto de perfil</p>
-	<form method="file" class="upload" enctype="multipart/form-data">
-		<div class="image">
-			<input type="file" name="image" class="form-control" required accept="*/image">
-		</div>
+<?php
+	if(isset($_POST['btn-add2'])){
+		$images=$_FILES['pfp']['name'];
+		$tmp_dir=$_FILES['pfp']['tmp_name'];
+		$imageSize=$_FILES['pfp']['size'];
+		$upload_dir=dirname(__DIR__).'../../pfp/';
+		$imgExt=strtolower(pathinfo($images,PATHINFO_EXTENSION));
+		$valid_extensions=array('jpeg', 'jpg', 'png');
+		$pic=rand(1000, 1000000).".".$imgExt;
+		// move_uploaded_file($tmp_dir, $upload_dir.$picProfile);
+		move_uploaded_file($tmp_dir, $upload_dir.$pic);
+		$stmt=$conn->prepare("UPDATE users SET pfp  = :upfp WHERE id = $user_id");
+		$stmt->bindParam(':upfp', $pic);
+		if($stmt->execute()){
+			echo('<script>alert("Imagen cargada exitosamente");</script>');
+		}
+	}
+?>
+		
+<div class="pfp">
+	<form method="post" class="upload" enctype="multipart/form-data">
+		<label class="label">Agregar foto de perfil</label>
+		<input type="file" name="pfp" class="form-control" required accept="*/image">
+
 		<div class="button2">
 			<button type="submit" class="btn" name="btn-add2">Subir</button></div>	
 		</div>			
 	</form>
 </div>
-
 
 <div class="container">
 	<div class="view-form">
@@ -76,3 +79,14 @@
 		</div>
 	</div>
 </div>
+
+<?php
+	$stmt=$conn->prepare('SELECT * FROM users ORDER BY id DESC');
+
+	$stmt->execute();
+	if($stmt->rowCount()>0)
+	{
+		$row=$stmt->fetch(PDO::FETCH_ASSOC);
+		echo '<img src="../../pfp/'.$row['pfp'].'">';
+	}
+?>
